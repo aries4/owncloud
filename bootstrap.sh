83a5882ef160d02ef62eb0c702654a2fa74a5cfd
aries4 <aries4gm@gmail.com>
2015-03-15 17:06:04 +0100

    bootstrap Apache2

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,14 +1,9 @@
 #!/bin/sh
 
 
-touch /var/log/cron/owncloud.log
-
-
 chown -R www-data:www-data /var/www/owncloud /owncloud
 echo "Starting server..\n"
 
-tail -F  /var/log/cron/owncloud.log &
 
 /usr/sbin/cron -f &
-/etc/init.d/php5-fpm start
 /etc/init.d/apache2 start
